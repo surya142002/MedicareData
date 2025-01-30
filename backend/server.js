673fe43a908894ef706c99626e203eb5754bbe7c
surya <surya142002@gmail.com>
2025-01-30 14:24:44 -0500

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 // Initialize models
